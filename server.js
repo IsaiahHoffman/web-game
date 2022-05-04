fc79f0e92096450c7a2a4765318a0b1633b65bfd
IsaiahHoffman <idhoffman@wisc.edu>
2022-05-04 18:03:39 -0500

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config()
+// }
 
 const express = require('express')
 const app = express()
@@ -12,9 +12,20 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL)
-//const db = mongoose.connection
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://idhoffman:IHbren0818!!@cluster0.2ijbu.mongodb.net/trial2?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log("Mongodb connected")
+  client.close();
+});
+
+
+// const mongoose = require('mongoose')
+// mongoose.connect(process.env.DATABASE_URL)
+// const db = mongoose.connection
 // db.on('error', error => console.error(error))
 // db.once('open', () => console.log('Connected to Mongoose'))
 
