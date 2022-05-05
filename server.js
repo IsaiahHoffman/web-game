1ee646673c11c2b54d0ff6e9c7e4c05fa98edec8
IsaiahHoffman <idhoffman@wisc.edu>
2022-05-04 20:25:54 -0500

    make it better

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 //     require('dotenv').config()
 // }
 
-// const express = require('express')
-// const app = express()
+const express = require('express')
+const app = express()
 // const expressLayouts = require('express-ejs-layouts')
 // const req = require('express/lib/request')
 
