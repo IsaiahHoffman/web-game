d2609e55c242526ea48e66eb7784aa30c3c9b758
IsaiahHoffman <idhoffman@wisc.edu>
2022-05-04 20:46:55 -0500

    make it better

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.NODE_ENV
+const uri = "mongodb+srv://idhoffman:IHbren0818!!@cluster0.2ijbu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
