05f7d03eb3dad2c4a6dbbfdb75e1099cd94448c9
IsaiahHoffman <idhoffman@wisc.edu>
2022-05-04 20:17:26 -0500

    make it better

--- server.js
+++ server.js
@@ -5,7 +5,10 @@
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-
+const req = require('express/lib/request')
+// app.get('/', (req, res) => {
+//     res.send("hello world")
+// })
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
@@ -22,11 +25,11 @@ client.connect(err => {
 });
 
 
-// const mongoose = require('mongoose')
-// mongoose.connect(process.env.DATABASE_URL)
-// const db = mongoose.connection
-// db.on('error', error => console.error(error))
-// db.once('open', () => console.log('Connected to Mongoose'))
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 const indexRouter = require("./routes/index")
 app.use('/', indexRouter)
